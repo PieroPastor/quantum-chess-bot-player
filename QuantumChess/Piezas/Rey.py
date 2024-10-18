13a6cc302a1878d37cc5d618a236e9fd6199e095
Piero Pastor <a20210836@pucp.edu.pe>
2024-10-18 15:35:25 -0500

    Fichas Completadas
    Falta elaborar el tablero cuántico

--- QuantumChess/Piezas/Rey.py
+++ QuantumChess/Piezas/Rey.py
@@ -27,30 +27,27 @@ class Rey(Pieza):
             self.movimientos.remove((0, 2))
             self.movimientos.remove((0, -2))
 
-    def EvaluarEnroque(self, tablero, objetivo):
+    def EvaluarEnroque(self, objetivo, tablero, piezas):
         fila, columna = objetivo
-        yA, xA = self.posicionReal
+        if self.color == BColors.BLACK and piezas[4].contadorMovimientos > 0: return False #Analiza al rey
+        if self.color == BColors.WHITE and piezas[20].contadorMovimientos > 0: return False
         global bandera_de_enroque
-        for mov in self.movimientos:
-            yM, xM = mov
-            if (yA + yM, xM + xA) == objetivo:
-                if columna == 2:
-                    pieza = tablero.tablero[fila][0]  # escoje la torre [1,2,3]
-                    if pieza.color == self.color and pieza.simbolo == 'R' and pieza.contadormovimientos == 0 and \
-                            tablero.tablero[fila][1] is None and tablero.tablero[fila][2] is None and \
-                            tablero.tablero[fila][3] is None:
-                        bandera_de_enroque = True
-                        return True
-                elif columna == 6:
-                    pieza = tablero.tablero[fila][7]  # escoje la torre [5  6]
-                    if pieza.color == self.color and pieza.simbolo == 'R' and pieza.contadormovimientos == 0 and \
-                            tablero.tablero[fila][5] is None and tablero.tablero[fila][6] is None:
-                        bandera_de_enroque = True
-                        return True
+        if columna == 2:
+            torre = tablero[fila][0] #Selecciona la torre
+            if torre == 'R' and piezas[(self.color == BColors.WHITE)*16+(self.color == BColors.BLACK)*0].contadorMovimientos == 0 and \
+                tablero[fila][1] == '.' and tablero[fila][2] == '.' and tablero[fila][3] == '.':
+                bandera_de_enroque = True
+                return True
+        elif columna == 6:
+            torre = tablero[fila][7]  # Selecciona la torre
+            if torre == 'R' and piezas[(self.color == BColors.WHITE)*23+(self.color == BColors.BLACK)*7].contadorMovimientos == 0 and \
+                tablero[fila][5] == '.' and tablero[fila][6] == '.':
+                bandera_de_enroque = True
+                return True
         bandera_de_enroque = False
         return False
 
     def MovimientoValido(self, tablero, origen, objetivo):
         y, x = objetivo
-        if (x == 2 or x == 6) and (y == 0 or y == 7): return self.EvaluarEnroque(tablero, objetivo)
-        else: return super().MovimientoValido(tablero, objetivo)
+        if (x == 2 or x == 6) and (y == 0 or y == 7): return self.EvaluarEnroque(objetivo, tablero.tablero, tablero.piezas)
+        else: return super().MovimientoValido(tablero.tablero, origen, objetivo)
