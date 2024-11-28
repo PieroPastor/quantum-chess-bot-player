b18eed22dd345b4757737cc6290b8a64f45a33ac
Piero Pastor <a20210836@pucp.edu.pe>
2024-11-28 11:04:21 -0500

    Implementación de IA, falta la función de predicción
    Falta terminar la función que usa calor para predecir

--- QuantumChess/Pantallas/AI.py
+++ QuantumChess/Pantallas/AI.py
@@ -1,3 +1,5 @@
+import threading
+
 from .Plays import *
 from Utils import *
 
@@ -20,11 +22,18 @@ def predict_move(model, board, turn, T):
     }
     raise NotImplementedError
 
+def predict_move_thread(model, board, turn, T, move, moves, done_flag, stopper):
+    while stopper[0] == False and move[0] not in moves:
+        pass
+        #move[0] = predict_move(model, board, turn, T)
+    done_flag[0] = True
+
 def main_ai(board, color):
     clock = pygame.time.Clock()
     # Temperatura para el ajuste
     T = 2.0  #Se variará dependiendo de los resultados obtenidos
     #model = tf.keras.models.load_model('Weights/weights.h5')
+    model = None
     input_boxes = [
         (pygame.Rect(50, 550, 100, 30), ""),  # Tipo de movimiento
         (pygame.Rect(160, 550, 50, 30), ""),  # Casilla 1
@@ -37,9 +46,12 @@ def main_ai(board, color):
     button_rect = pygame.Rect(235, 600, 100, 30)  # Botón de enviar
     error_message = ""
     turn = "W"
-    ai = "B" if color == 1 else "W"
     player = "W" if color == 1 else "B"
     moves = board.GetMoves(turn)
+    thread = None
+    move_ai = [None]  # Contenedor para la predicción
+    done_flag = [False]
+    stopper = [False]
     while True:
         screen.fill(GRAY)  # Fondo gris claro
 
@@ -59,39 +71,81 @@ def main_ai(board, color):
         if error_message:
             draw_error_message(error_message)
 
-        #if player == turn:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                stopper[0] = True
+                thread.join()
                 pygame.quit()
                 sys.exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if back_button_rect.collidepoint(event.pos):
+                    stopper[0] = True
+                    thread.join()
                     return
-                # Verificar si se seleccionó una caja de texto
-                for i, (rect, _) in enumerate(input_boxes):
-                    if rect.collidepoint(event.pos):
-                        active_index = i
-                        input_boxes[active_index] = (rect, "")  # Limpiar caja de texto
-                        break
-                else:
-                    active_index = -1
 
-                # Verificar si se presionó el botón
-                if button_rect.collidepoint(event.pos):
-                    if validate_input_boxes(input_boxes):
-                        neo_move = serialize(input_boxes, turn)
-                        if neo_move in moves:
-                            board.GenericMove(neo_move)
-                            turn = "B" if turn == "W" else "W"
-                            moves = board.GetMoves(turn)
-                            error_message = ""  # Borrar mensaje de error
-                        else: error_message = "Movimiento inválido."
-                    else:
-                        error_message = "Por favor, complete todos los campos correctamente."
-                if back_button_rect.collidepoint(event.pos):
-                    return
+            #Analiza jugadas del player
+            if turn == player:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # Verificar si se seleccionó una caja de texto
+                    for i, (rect, _) in enumerate(input_boxes):
+                        if rect.collidepoint(event.pos):
+                            active_index = i
+                            input_boxes[active_index] = (rect, "")  # Limpiar caja de texto
+                            break
+                        else:
+                            active_index = -1
+
+                        # Verificar si se presionó el botón
+                        if button_rect.collidepoint(event.pos):
+                            if validate_input_boxes(input_boxes):
+                                neo_move = serialize(input_boxes, turn)
+                                if neo_move in moves:
+                                    board.GenericMove(neo_move)
+                                    turn = "B" if turn == "W" else "W"
+                                    moves = board.GetMoves(turn)
+                                    error_message = ""  # Borrar mensaje de error
+                                else: error_message = "Movimiento inválido."
+                            else:
+                                error_message = "Por favor, complete todos los campos correctamente."
+
+                            #Llenado de datos si es el turno
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:  # Detectar Enter
+                        # Simula el clic del botón de enviar
+                        if validate_input_boxes(input_boxes):
+                            neo_move = serialize(input_boxes, turn)
+                            if neo_move in moves:
+                                board.GenericMove(neo_move)
+                                turn = "B" if turn == "W" else "W"
+                                moves = board.GetMoves(turn)
+                                error_message = ""  # Borrar mensaje de error
+                            else:
+                                error_message = "Movimiento inválido."
+                        else:
+                            error_message = "Por favor, complete todos los campos."
+                    elif active_index != -1:
+                        # Manejo de entrada en las cajas de texto
+                        _, text = input_boxes[active_index]
+                        if event.key == pygame.K_BACKSPACE:
+                            input_boxes[active_index] = (input_boxes[active_index][0], text[:-1])
+                        elif len(text) < max_lengths[active_index]:  # Límite de caracteres
+                            input_boxes[active_index] = (input_boxes[active_index][0], text + event.unicode)
+
+        #Lanza el hilo si no hay hilo activo y no es el turno del jugador
+        if not turn == player and thread == None:
+            thread = threading.Thread(target=predict_move_thread, args=(model, board.tablero, turn, T, move_ai, moves, done_flag, stopper))
+            thread.start()
+
+        #Si ya encontró una jugada posible, la realiza
+        if done_flag[0]:
+            thread.join()
+            move_ai = move_ai[0]
+            board.GenericMove(move_ai)
+            turn = "B" if turn == "W" else "W"
+            moves = board.GetMoves(turn)
+            done_flag[0] = False
+            thread = None
 
         pygame.display.flip()
         clock.tick(60)
